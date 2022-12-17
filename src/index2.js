cd24d956037c64700d60ecef04a08d66d43a45aa
Jim Blackler <jimblackler@google.com>
2022-12-17 20:20:22 +0000

    New demo.

---
+++ src/index2.js
@@ -0,0 +1,54 @@
+const treefun2 = require('treefun2');
+
+const tree = [{
+  label: 'World',
+  children: [{
+    label: 'Europe',
+    children: [{
+      label: 'France',
+    }, {
+      label: 'Germany'
+    }]
+  }]
+}];
+
+const options = {
+  flipXY: false,
+  width: 320,
+  height: 450,
+  labelLineSpacing: 15,
+  labelPadding: 2,
+  arrowHeadWidth: 5,
+  arrowHeadHeight: 5,
+  arrowsUp: false,
+  minimumSiblingGap: 0.1,
+  idealSiblingGap: 0.1,
+  minimumCousinGap: 0.2,
+  idealCousinGap: 1.2,
+  levelsGap: 1.2,
+  minimumDepth: 0,
+  minimumBreadth: 0
+};
+
+const css = `
+  text {
+    text-anchor: middle;
+    font-size: x-small;
+  }
+  
+  rect {
+    fill: ghostwhite;
+    rx: 20;
+    stroke: black;
+    stroke-width: 0.3px;
+  }
+  
+  line {
+    stroke: black;
+    opacity: 0.5;
+    stroke-width: 0.3px;
+  }
+`;
+
+treefun2.treeToDiagram(document, document.body, tree, options, css);
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
 
-module.exports = {
+module.exports = [{
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'site/dist')
   }
-};
+},
+  {
+    entry: './src/index2.js',
+    output: {
+      filename: 'main2.js',
+      path: path.resolve(__dirname, 'site/dist')
+    }
+  }];
